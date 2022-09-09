b60c2a1807454282b60f9cb3da7e40f2a4cc0e25
Andrew <andrewboggio@gmail.com>
2022-09-09 14:32:07 -0400

    Fixed blank command on line 8

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -5,9 +5,9 @@ import java.util.stream.Collectors;
 public class Dictionary {
     public static void main(String[] args) {
         String word = JOptionPane.showInputDialog("Enter a word:");
-        String command = "" + word;
+        String command = "curl -X GET https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
         Runtime rt = Runtime.getRuntime();
-        try{Process pr = rt.exec("curl -X GET https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+        try{Process pr = rt.exec(command);
 
 //Java 8 version
         String result = new BufferedReader(
